8f037adf3aaba1692f6c873ea758c2ee18c90274
vishambersaini <vks9694903585@gmail.com>
2023-12-19 18:08:27 +0530

    Create Server in Index file

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("welcome NodeJS")
+require('dotenv').config()
+const express=require('express')
+
+const app=express()
+const Port=process.env.PORT||5000
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+app.listen(Port,()=>{console.log("App listing on Port:",Port);})
+console.log(process.env)
